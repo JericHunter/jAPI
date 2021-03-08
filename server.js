5382a199e4bf737b3a5938d08b687e835d893a9b
JericHunter <jeric.hunterr@gmail.com>
2021-03-08 01:01:00 -0500

    ff

--- server.js
+++ server.js
@@ -1,40 +1,16 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+const util = require('util');
+const app = require('./config/express');
+const router = require('./controllers/index.js');
+require('./data/api-db');
+mongoose.Promise = Promise;
 
-const express = require('express');
-var cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
-const MongoClient = require('mongodb').MongoClient;
-const app = express();
+app.use(router);
 
-const bodyParser = require('body-parser');
-const expressValidator = require('express-validator');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(expressValidator());
-app.use(cookieParser());
-
-require('./data/db');
-
-var checkAuth = (req, res, next) => {
-  console.log("Checking authentication");
-  if (typeof req.cookies.nToken === "undefined" || req.cookies.nToken === null) {
-    req.user = null;
-  } else {
-    var token = req.cookies.nToken;
-    var decodedToken = jwt.decode(token, { complete: true }) || {};
-    req.user = decodedToken.payload;
-  }
-
-  next();
-};
-app.use(checkAuth);
-
-
-// TODO: Add each controller here, after all middleware is initialized.
-
-const port = 8000;
-require('./app/routes')(app, {});app.listen(port, () => {  console.log('We are live on ' + port);});
-
-module.exports = app;
+if (!module.parent) {
+    const port = process.env.PORT
+    app.listen(process.env.PORT, () => {
+        console.log(`app listening on port ${port}`);
+    });
+}
