fb06f5a813e0dc29a84b87783f74226808b0f424
JericHunter <jeric.hunterr@gmail.com>
2021-03-08 01:12:43 -0500

    auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,68 @@ const User = require('../models/user.js');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 
-module.exports = (app) => {
-   // TODO: Implement authentication controller.
-  }
+// SIGN UP FORM
+// router.get("/sign-up", (req, res) => {
+//     res.render("sign-up");
+// });
+
+// SIGN UP
+router.post("/sign-up", (req, res) => {
+    // Create User and JsonWebToken
+    const user = new User(req.body);
+
+    user.save().then(user => {
+        const token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
+        res.cookie('nToken', token, { maxAge: 900000 , httpOnly: true });
+        res.json({'jwt-token': token})
+        console.log("new sign-up token: " + token)
+        // res.redirect("/");
+        }).catch(err => {
+        console.log(err.message);
+        return res.status(400).send({ err: err });
+        });
+})
+
+// LOGIN FORM
+// router.get('/login', (req, res) => {
+//     res.render('login');
+// });
+
+// LOGIN
+router.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // find the username
+    User.findOne({ username }, "username password")
+        .then(user => {
+            if (!user){
+            // user not found
+            return res.status(401).send({ message: "wrong username or password" });
+            }
+            // check the password
+            user.comparePassword(password, (err, isMatch) => {
+            if (!isMatch) {
+                return res.status(401).send({ message: "wrong username or password" });
+            }
+            // create a token
+            const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+                expiresIn: "60 days"
+            });
+            // set a cookie and redirect to root
+            res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+            res.json({'login:': 'great success', 'user-signed-in:': user})
+            // res.redirect("/");
+            });
+        }).catch(err => {
+            console.log(err);
+        });
+});
+
+// LOGOUT
+router.get('/logout', (req, res) => {
+    res.clearCookie('nToken');
+    res.redirect('/');
+});
+
+module.exports = router;
---
+++ models/quotes.js
@@ -0,0 +1,26 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const QuoteSchema = new Schema({
+    author: { type: String, required: true },
+    quote: { type: String, required: true },
+    year: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now},
+    updatedAt: { type: Date},
+});
+
+QuoteSchema.pre("save", function(next) {
+    // SET createdAt AND updatedAt
+    const now = new Date();
+    this.updatedAt = now;
+
+    if (!this.createdAt) {
+      this.createdAt = now;
+    }
+
+    next();
+});
+
+Quotw = mongoose.model("Quote", QuoteSchema);
+module.exports = Quote;
