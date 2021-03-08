f509ca84edcad2ca7abf640077e11ea0030a77c5
JericHunter <jeric.hunterr@gmail.com>
2021-03-08 01:08:53 -0500

    vv

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
-const User = require("../models/user");
+const express = require('express');
+const User = require('../models/user.js');
 const jwt = require('jsonwebtoken');
-
+const router = express.Router();
 
 module.exports = (app) => {
    // TODO: Implement authentication controller.
